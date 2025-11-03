34135fc9d4ff8fc18c5dd0cacfdfd6e449dd9e38
sabriyasar <info@sabriyasar.com.tr>
2025-11-03 10:17:04 +0300

    updated 33

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -35,7 +35,7 @@ export default function AdminPage() {
 
   useEffect(() => {
     // Socket.io bağlantısı
-    const s: Socket = io(process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5050')
+    const s: Socket = io(process.env.NEXT_PUBLIC_API_URL)
 
     // Sunucudan online kullanıcı sayısını dinle
     s.on('online_users', (count: number) => {
